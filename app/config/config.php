0fc48c1b82ede85259f770f7f3d49601f2aa79ad
George <admin@georgesimos.com>
2020-04-05 14:39:26 +0300

    init

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php 
+    // DB Params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', 'root');
+    define('DB_NAME', 'shareposts');
+    
+    // App Root
+    // echo __FILE__;
+    define('APPROOT', dirname(dirname(__FILE__)));
+    // URL Root
+    define('URLROOT', 'http://localhost:8888/sharepost/');
+    // Site Name
+    define('SITENAME', 'Share Posts');
