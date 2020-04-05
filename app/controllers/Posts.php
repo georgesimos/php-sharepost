cb2dfeb1381582b757b5d9d4002cf2c0097e383a
George <admin@georgesimos.com>
2020-04-05 20:19:35 +0300

    add new post

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -1,14 +1,16 @@
 <?php
-class Posts extends Controller {
+class Posts extends Controller
+{
     public function __construct()
     {
-        if(!isLoggedIn()) {
+        if (!isLoggedIn()) {
             redirect('users/login');
         }
 
         $this->postModel = $this->model('Post');
     }
-    public function index() {
+    public function index()
+    {
         // Get Posts
         $posts = $this->postModel->getPosts();
 
@@ -18,11 +20,48 @@ class Posts extends Controller {
         $this->view('posts/index', $data);
     }
 
-    public function add() {
-               $data = [
-            'title' => '',
-            'body' => ''
-        ];
+    public function add()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // Sanitize POST array
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+                'title' => trim($_POST['title']),
+                'body' => trim($_POST['body']),
+                'user_id' => $_SESSION['user_id'],
+                'title_error' => '',
+                'body_error' => '',
+            ];
+
+            // Validate title 
+            if (empty($data['title'])) {
+                $data['title_error'] = 'Please enter title';
+            }
+            // Validate body 
+            if (empty($data['body'])) {
+                $data['body_error'] = 'Please enter body';
+            }
+
+            // Make sure no errors
+            if (empty($data['title_error']) && empty($data['body_error'])) {
+                // Validated
+                if($this->postModel->addPost($data)) {
+                    flash('post_message', 'Post Added');
+                    redirect('posts');
+                } else {
+                    die('Something Went Wrong');
+                }
+            } else {
+                // Load view with errors
+                $this->view('posts/add', $data);
+            }
+        } else {
+            $data = [
+                'title' => '',
+                'body' => ''
+            ];
+        }
         $this->view('posts/add', $data);
     }
 }
--- app/models/Post.php
+++ app/models/Post.php
@@ -27,4 +27,22 @@ class Post {
     return $results;
 
   }
+
+
+  // Add post
+  public function addPost($data)
+  {
+    $this->db->query('INSERT INTO posts (title, body, user_id) VALUES(:title, :body, :user_id)');
+    // Bind values
+    $this->db->bind(':title', $data['title']);
+    $this->db->bind(':body', $data['body']);
+    $this->db->bind(':user_id', $data['user_id']);
+
+    // Execute
+    if ($this->db->execute()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
