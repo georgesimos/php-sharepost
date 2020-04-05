88479823170266e1bb8216e86dcafd84af5091f5
George <admin@georgesimos.com>
2020-04-05 19:26:35 +0300

    complete user auth

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -135,7 +135,7 @@ class Users extends Controller
 
                 if ($loggedInUser) {
                     // Create Session
-                    die('SUCCESS');
+                    $this->createUserSession($loggedInUser);
                 } else {
                     $data['password_error'] = 'Password incorerect';
 
@@ -157,5 +157,29 @@ class Users extends Controller
             // Load view 
             $this->view('users/login', $data);
         }
+    }
+
+    public function createUserSession($user) {
+        $_SESSION['user_id'] = $user->id;
+        $_SESSION['user_email'] = $user->email;
+        $_SESSION['user_name'] = $user->name;
+        redirect('/pages/index');
+    }
+
+    public function logout() {
+        unset($_SESSION['user_id']);
+        unset($_SESSION['user_email']);
+        unset($_SESSION['user_name']);
+        session_destroy();
+        redirect('/users/login');
+    }
+
+    public function isLoggedIn()
+    {
+       if(isset($_SESSION['user_id'])) {
+           return true;
+       } else {
+           return false;
+       }
     }
 }
