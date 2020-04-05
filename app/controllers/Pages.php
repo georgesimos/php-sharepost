cc44446ac16c1eea5f47613e46307c2c2e407133
George <admin@georgesimos.com>
2020-04-05 20:08:43 +0300

    create Post form

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -8,6 +8,9 @@ class Pages extends Controller
 
     public function index()
     {
+        if(isLoggedIn()) {
+            redirect('posts');
+        }
         $data = ['title' => 'Share Posts', 'description' => 'Simple social network build on the SimosMVC PHP Framework'];
         $this->view('pages/index', $data);
     }
--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -17,4 +17,12 @@ class Posts extends Controller {
         ];
         $this->view('posts/index', $data);
     }
+
+    public function add() {
+               $data = [
+            'title' => '',
+            'body' => ''
+        ];
+        $this->view('posts/add', $data);
+    }
 }
